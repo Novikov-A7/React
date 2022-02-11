f0358e30d6b30105bd707c48c523562ee237a3f2
Novikov-A7 <novikov_andrey.7@mail.ru>
2022-02-11 19:58:16 +0300

    Homework3 completed

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,15 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
-import {Form} from './components/Form';
+import { useRef, useState } from "react";
+//import {Form} from './components/Form';
 import { AUTHORS } from "./utils/constants.js";
-import { useEffect } from "react";
+import { useEffect } from "react";  
 import { MessageList } from './components/MessageList/index.js';
-
+import { FormMui } from './components/Form/FormMui/index.js';
+import { ChatList } from './components/ChatsList/index.js';
 
 
 function App() {
   const [messageList, setMessageList] = useState([]);
+  const messagesEnd = useRef();
+  
 
   const handleAddMessage = (text) => {
     sendMessage (text, AUTHORS.ME)
@@ -19,35 +22,38 @@ function App() {
     const newMsg = {
       text,
       author,
+      id: `msg-${Date.now()}`,
     };
     setMessageList((prevMessageList) => [...prevMessageList, newMsg]);
   };
 
   useEffect(() => {
+    messagesEnd.current?.scrollIntoView();
     let timeBreak;
     if(messageList[messageList.length - 1]?.author === AUTHORS.ME) {
       timeBreak = setTimeout(() => {
         sendMessage("I'll be back soon", AUTHORS.BOT)
       }, 1000)
     };
-    return () => {clearTimeout(timeBreak)};
+    return () => clearTimeout(timeBreak);
   }, [messageList]);
 
   return(
-    <>
-      <h2>FRAGMENT</h2>
       <div className="App">
-
-        <header className="App-header">
-          <MessageList messages={messageList} />
-
-
-          <Form onSubmit={handleAddMessage}/>
-
-        </header>
+              
+        <div className="App-content">
+          <div className='wrapper'>
+            <div className='myChatList'>
+              <ChatList/>
+            </div>
+            <div className='chatWindow'>
+              <MessageList  messages={messageList} />
+              <div ref={messagesEnd}></div>
+              <FormMui onSubmit={handleAddMessage}/>
+            </div>  
+          </div>
+        </div>
       </div>
-    </>
-    
   )
 };
 
--- messenger/src/components/MessageList/index.js
+++ messenger/src/components/MessageList/index.js
@@ -3,6 +3,7 @@ import { Message } from "../Message/index.js";
 export const MessageList = ({messages}) => 
   messages.map((message) => ( 
     <Message 
+      key={message.id}
       text = {message.text}
       author = {message.author}
     />
